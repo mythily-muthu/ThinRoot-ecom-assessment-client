0a7a17e8f72217a4475f3a19ff7a323caa7edbf2
mythily-muthu <mythilimuthu18@gmail.com>
2023-05-25 18:23:16 +0530

    Fix: Added error message to register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,36 +2,42 @@ import React from "react";
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  let navigate = useNavigate();
+  // initial values
   let initialValues = {
     name: "",
     email: "",
     password: "",
     confirmpassword: "",
   };
+  // register schema
   let registerSchema = Yup.object().shape({
     name: Yup.string()
       .required("Please Enter UserName")
-      .min(6, "Username length should be more than 5"),
+      .min(3, "Username length should be more than 2"),
     email: Yup.string()
       .email("*should be valid email")
       .required("*Please enter email!"),
 
     password: Yup.string()
-      .min(4, "*Password should contain more than 3 characters.")
-      .max(9, "*Password shouldn't exceed 9 characters.")
+      .min(5, "*Password should contain more than 4 characters.")
+      .max(40, "*Password shouldn't exceed 40 characters.")
       .required("*Please enter Password!"),
   });
 
-  const handleSubmit = async (values) => {
-    console.log("values", values);
+  const handleSubmit = async (values, { resetForm }) => {
+    // console.log("values", values);
     let registerValues = { ...values };
     delete registerValues.confirmpassword;
     console.log("registerValues", registerValues);
     let baseUrl = "http://localhost:8000/api/";
     let res = await axios.post(`${baseUrl}auth/register`, registerValues);
     console.log(res.data);
+    resetForm();
+    alert("User created successfully");
   };
 
   return (
@@ -42,11 +48,13 @@ const Register = () => {
       }}
     >
       <div
-        className="flex flex-col h-auto w-auto justify-center items-center bg-white p-12 border border-primary
+        className="flex flex-col h-auto w-[500px] justify-center items-center bg-white p-12 border border-yellow
         bg-opacity-25"
       >
-        <p className="font-bold text-primary">MAKE YOU UP</p>
-        <p className="font-medium py-3">Register</p>
+        <p className="font-bold text-xl text-primary tracking-[0.2em]">
+          MAKE YOU UP
+        </p>
+        <p className="font-light py-3 uppercase tracking-[0.2em]">Register</p>
         <Formik
           initialValues={initialValues}
           validationSchema={registerSchema}
@@ -57,7 +65,10 @@ const Register = () => {
               <Form>
                 {/* name */}
                 <div className="py-4 flex flex-col gap-y-3  ">
-                  <label htmlFor="name" className="font-medium text-primary">
+                  <label
+                    htmlFor="name"
+                    className="font-light tracking-[0.2em] text-primary"
+                  >
                     Name
                   </label>
                   <Field
@@ -73,7 +84,7 @@ const Register = () => {
                 <div className="py-4 flex flex-col gap-y-3 ">
                   <label
                     htmlFor="Password"
-                    className="font-medium text-primary"
+                    className="font-light tracking-[0.2em] text-primary"
                   >
                     Email
                   </label>
@@ -90,7 +101,7 @@ const Register = () => {
                 <div className="py-4 flex flex-col gap-y-3 ">
                   <label
                     htmlFor="Password"
-                    className="font-medium text-primary"
+                    className="font-light tracking-[0.2em] text-primary"
                   >
                     Password
                   </label>
@@ -107,24 +118,31 @@ const Register = () => {
                 <div className="py-4 flex flex-col gap-y-3 ">
                   <label
                     htmlFor="Password"
-                    className="font-medium text-primary"
+                    className="font-light tracking-[0.2em] text-primary"
                   >
                     Confirm Password
                   </label>
                   <Field
                     name="confirmpassword"
-                    type="confirmpassword"
+                    type="password"
                     placeholder="Confirm Password"
                     className="border border-black px-2 flex flex-col py-1"
                   />
                 </div>
-
-                <button
-                  className="flex justify-center items-center bg-primary text-white px-4 py-1  rounded-md"
-                  type="submit"
+                <div className="flex justify-center py-4">
+                  <button
+                    className="flex justify-center items-center bg-primary text-white px-4 py-1 font-medium rounded-md tracking-[0.2em]"
+                    type="submit"
+                  >
+                    REGISTER
+                  </button>
+                </div>
+                <p
+                  className="flex w-full font-normal text-sm cursor-pointer"
+                  onClick={() => navigate("/login")}
                 >
-                  REGISTER
-                </button>
+                  Registered already? click here to LOGIN
+                </p>
               </Form>
             );
           }}
