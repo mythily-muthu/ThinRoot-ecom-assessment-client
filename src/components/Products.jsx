526eab1ddf30032564ad39b47514ebf6fd3ed1d1
mythily-muthu <mythilimuthu18@gmail.com>
2023-05-25 15:49:02 +0530

    Created order page using router

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,28 +1,57 @@
 import React from "react";
 import products from "../data.json";
+import { useNavigate } from "react-router-dom";
+import Button from "./Button";
 
 const Products = () => {
+  let navigate = useNavigate();
+  const handleClick = (productId) => {
+    navigate(`/product/${productId}`);
+  };
+
   return (
-    <div className="flex w-full h-full ">
+    <div className="flex w-full min-h-screen ">
       <div className="flex flex-col w-[80%] h-full mx-auto ">
-        <p>Pick up where you left off</p>
-        <div>
-          {products.map((item) => {
-            return (
-              <div className="w-52 h-72 flex flex-col" key={item._id}>
-                <img
-                  src={item.image_link}
-                  alt={item.name}
-                  className="w-52 h-72 object-cover"
-                />
-                <div>
-                  <p>{item.name}</p>
-                  <p>{item.price}</p>
-                  <button className="p-3 text-white">BUY NOW</button>
+        <div className="pt-12 flex flex-col gap-y-10 justify-center">
+          <p
+            className="flex text-4xl"
+            style={{
+              fontFamily: "Trirong",
+            }}
+          >
+            Pick up where you left off
+          </p>
+
+          <div className=" flex flex-wrap w-full gap-x-20  h-full items-center justify-center">
+            {products.map((item) => {
+              return (
+                <div
+                  className="flex w-[20%] py-10"
+                  key={item._id}
+                  onClick={() => handleClick(item._id)}
+                >
+                  <div className="flex flex-col ">
+                    <img
+                      src={item.image_link}
+                      alt={item.name}
+                      className="w-80 h-[500px] object-contain rounded-xl mx-auto border border-yellow"
+                    />
+                    <div
+                      className="flex flex-col py-5 gap-y-2 text-lg uppercase tracking-[0.1em] "
+                      style={{
+                        fontFamily: "Trirong",
+                      }}
+                    >
+                      <p className="">{item.name}</p>
+                      <p className="">${item.price}</p>
+                    </div>
+
+                    <Button name={"BUY NOW"} />
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,7 +1,80 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import Topbar from "../components/Topbar";
+import Navbar from "../components/Navbar";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const SingleProduct = () => {
-  return <div>SingleProduct</div>;
+  const [productDetails, setProductDetails] = useState({});
+  let params = useParams();
+  console.log("params", params);
+
+  let baseUrl = "https://mythu-ecommerce-app.onrender.com/product/find";
+
+  const getProductDetails = async () => {
+    let res = await axios.get(`${baseUrl}/${params.id}`);
+    setProductDetails(res.data);
+  };
+
+  useEffect(() => {
+    getProductDetails();
+  }, []);
+  console.log("productDetails", productDetails);
+
+  return (
+    <div className="flex flex-col h-full w-full ">
+      <Topbar />
+      <Navbar />
+
+      <div className="flex w-[73%] mx-auto h-full justify-center py-16">
+        <div className="flex w-1/2 h-full items-center  ">
+          <div className="w-full flex justify-center">
+            <img
+              src={productDetails.image_link}
+              alt={productDetails.name}
+              className="w-[500px] border border-yellow"
+            />
+          </div>
+        </div>
+
+        <div className="w-1/2  h-full">
+          <div
+            className="flex flex-col gap-y-5 p-3 "
+            style={{
+              fontFamily: "Trirong",
+            }}
+          >
+            <p className="font-semibold text-4xl tracking-[0.1em] items-baseline border-b-2 border-yellow ">
+              {productDetails.name}
+            </p>
+            <p className="font-medium text-2xl tracking-[0.2em] py-4">
+              ${productDetails.price}
+            </p>
+            <p className="font-bold text-base tracking-[0.2em] ">SELECT SIZE</p>
+            <div className="flex gap-x-5 tracking-[0.2em] pb-7 ">
+              <p className="font-medium text-lg tracking-[0.2em] ">S</p>
+              <p className="font-medium text-lg tracking-[0.2em]">M</p>
+              <p className="font-medium text-lg tracking-[0.2em]">L</p>
+              <p className="font-medium text-lg tracking-[0.2em]">XL</p>
+              <p className="font-medium text-lg tracking-[0.2em]">XXL</p>
+            </div>
+            <div className="flex gap-x-5 tracking-[0.2em]">
+              <p className="font-bold ">QUANTITY</p>
+            </div>
+            <div className=" flex gap-5">
+              <button className="p-3 border border-black font-semibold tracking-[0.2em] rounded-3xl px-7">
+                ADD TO WISHLIST
+              </button>
+              <button className="p-3 bg-primary text-white font-semibold tracking-[0.2em] rounded-3xl px-7">
+                ADD TO BAG
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SingleProduct;
