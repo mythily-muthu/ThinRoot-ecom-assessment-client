e9cea0fdcb288f742e2cfb73fdd7289963e3ab76
mythily-muthu <mythilimuthu18@gmail.com>
2023-05-26 12:50:27 +0530

    Feat:  Slices are implemented, Feat: global store state using Redux implemented.

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,14 +1,35 @@
-import React from "react";
-import products from "../data.json";
+import React, { useEffect, useState } from "react";
+
 import { useNavigate } from "react-router-dom";
-import Button from "./Button";
+import LoadingSpinner from "./LoadingSpinner";
+import { publicRequest } from "../axiosMethod";
 
 const Products = () => {
+  const [loading, setLoading] = useState(true);
+  // const [products, setProducts] = useState([]);
   let navigate = useNavigate();
   const handleClick = (productId) => {
     navigate(`/product/${productId}`);
   };
 
+  const getAllProducts = async () => {
+    try {
+      let res = await publicRequest.get("/products");
+      if (res.status === 200) {
+        setProducts(res.data.products);
+      }
+    } catch (error) {
+      console.log("error", error.message);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getAllProducts();
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   return (
     <div className="flex w-full min-h-screen ">
       <div className="flex flex-col w-[80%] h-full mx-auto ">
@@ -22,27 +43,31 @@ const Products = () => {
             Pick up where you left off
           </p>
 
-          <div className=" flex flex-wrap w-full gap-x-20  h-full items-center justify-center">
-            {products.map((item) => {
-              return (
-                <div
-                  className="flex w-[20%] py-10"
-                  key={item._id}
-                  onClick={() => handleClick(item._id)}
-                >
-                  <div className="flex flex-col ">
-                    <img
-                      src={item.image_link}
-                      alt={item.name}
-                      className="w-80 h-[500px] object-contain rounded-xl mx-auto border border-yellow"
-                    />
+          {loading ? (
+            <LoadingSpinner />
+          ) : (
+            <div className=" flex flex-wrap justify-center w-full gap-5 h-full ">
+              {products.map((item) => {
+                return (
+                  <div
+                    className="flex flex-col  gap-y-1.5 w-[300px] h-full"
+                    key={item._id}
+                    onClick={() => handleClick(item._id)}
+                  >
+                    <div className="w-full h-[300px] ">
+                      <img
+                        src={item.image}
+                        alt={item.title}
+                        className="h-full w-full object-contain rounded-xl p-4 border border-yellow"
+                      />
+                    </div>
                     <div
                       className="flex flex-col py-5 gap-y-1 text-lg uppercase tracking-[0.1em] "
                       style={{
                         fontFamily: "Trirong",
                       }}
                     >
-                      <p className="font-extrabold">{item.name}</p>
+                      <p className="font-extrabold">{item.title}</p>
                       <p className="font-medium tracking-[0.4em]">
                         ${item.price}
                       </p>
@@ -51,10 +76,10 @@ const Products = () => {
                       </p>
                     </div>
                   </div>
-                </div>
-              );
-            })}
-          </div>
+                );
+              })}
+            </div>
+          )}
         </div>
       </div>
     </div>
